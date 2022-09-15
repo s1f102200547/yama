b4a01ce6a512aa7d7ee5ba476ebaef5c943cc25b
s1f102200547 <s1f102200547@iniad.org>
2022-09-15 14:12:33 +0900

    電話番号完成

--- app.py
+++ app.py
@@ -2,18 +2,17 @@ import re
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/', methods=["POST"])
-def top():
-    global result
+@app.route('/')
+def top(): 
+    return render_template('index.html')
+
+@app.route("/result", methods=["POST"])
+def result():
     if re.fullmatch("(070|080|090)-[0-9]{4}-[0-9]{4}", request.form.get("tell")):
         result = "完了"
     else:
         result = "失敗" 
-    return render_template('index.html')
-
-@app.route("/result")
-def result():
-    return render_template(".html", result=result)
+    return render_template("index2.html", result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
